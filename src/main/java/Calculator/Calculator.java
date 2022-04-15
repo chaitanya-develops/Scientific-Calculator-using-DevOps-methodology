0ed8a9cda58b8fc7d1e9feab029a1851ec925c8a
chaitanya-develops <krishnachaitanya.reddy@iiitb.ac.in>
2022-04-16 00:29:38 +0530

    Add exception catch to main menu

--- src/main/java/Calculator/Calculator.java
+++ src/main/java/Calculator/Calculator.java
@@ -1,6 +1,8 @@
 package Calculator;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 
@@ -18,7 +20,15 @@ public class Calculator {
             System.out.println("Choose an operation : ");
             System.out.println("1. Square Root");
             System.out.println("2. Exit");
-            choice = input.nextInt();
+            try {
+                choice = input.nextInt();
+            }
+            catch (InputMismatchException ex) {
+                System.out.println("Wrong Input Choose from the given option only.");
+                logger.error(ex);
+                break;
+            }
+
             switch (choice){
                 case 1:
                     double number;
@@ -28,6 +38,7 @@ public class Calculator {
                     System.out.println("----------------------------------------------------------------");
                     System.out.println(" ");
                     System.out.println("Enter a positive number to calculate square root : ");
+
                     number = input.nextDouble();
                     result = calculator.squareRoot(number);
 //                    System.out.println("The square root of [" + number + "] is [" + result + "]");
